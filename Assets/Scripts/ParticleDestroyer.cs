6d0be5f281203b3edda5271d72417a010fc66221
gkaragoz <gkaragoz@gmail.com>
2018-10-13 02:14:39 +0300

    Create ParticleDestroyer.cs

---
+++ Assets/Scripts/ParticleDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestroyer : MonoBehaviour {
+
+    public float lifeTime;
+
+    private ParticleSystem FX;
+
+	void Start () {
+        FX = GetComponent<ParticleSystem>();
+
+        lifeTime = FX.main.startLifetime.constant;
+
+        DestroyItself();
+	}
+	
+    private void DestroyItself() {
+        Destroy(gameObject, lifeTime);
+    }
+}
