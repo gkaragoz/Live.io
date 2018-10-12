317d8d76daa7d485bdc6173e0491410ff386f893
gkaragoz <gkaragoz@gmail.com>
2018-10-13 00:50:34 +0300

    Rename PlayerMovement.cs to PlayerController.cs and complete it.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 5f;
+
+    private Rigidbody2D _rb;
+
+    void Start() {
+        _rb = GetComponent<Rigidbody2D>();    
+    }
+
+    void FixedUpdate() {
+        Aim();
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 targetVelocity = new Vector2(horizontal, vertical);
+        _rb.velocity = targetVelocity * speed;
+    }
+
+    void Aim() {
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 lookAtDirection = Input.mousePosition - objectPos;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(lookAtDirection.y, lookAtDirection.x) * Mathf.Rad2Deg));
+    }
+}
